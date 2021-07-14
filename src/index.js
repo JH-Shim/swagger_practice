45d163045f22742804f19763fc461bf451cd563d
slogup-jhshim <jhshim@slogup.com>
2021-07-14 15:43:22 +0900

    YAML

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ const swaggerUi = require('swagger-ui-express')
 const YAML = require('yamljs')
 
 const usersRouter = require('./routers/usersRouter')
-const apiSpecification = YAML.load('./src/openapi-sample.yaml') // 루트 디렉토리를 기준으로 상대경로
+const apiSpecification = YAML.load('./src/openapi-sample.yaml') // 루트 디렉토리를 기준으로 상대경로를 적는다.
+// ! 위의 apiSpecification은 공식 문서에서 제공하는 샘플입니다. 아래의 apiSpecification은 해당 프로젝트의 것입니다.
+// const apiSpecification = YAML.load('./src/originalApiSpecification.yaml')
 
 const app = express()
 const PORT = 5000
